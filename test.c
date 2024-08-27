259a1436663e5a53260e0587875da312ce02b36a
Dibyajyoti-08 <eng.djena075@gmail.com>
2024-08-27 12:58:42 +0530

    added a new print line in the test.c

--- test.c
+++ test.c
@@ -1,7 +1,8 @@
 # include<stdio.h>
 int main() {
 	printf("This is the Git test.");
-	print("Small change occured!");
-	print("new change.");
+	printf("Small change occured!");
+	printf("new change.");
+	printf("Added a new line.");
 	return 0;
 }
